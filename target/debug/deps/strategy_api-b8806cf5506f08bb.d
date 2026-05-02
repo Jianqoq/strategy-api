4fdcd497f052a4c6065f203d2c00b895094e34ed
JianJian Li <ljj1849532909@gmail.com>
2026-05-02 14:20:55 -0400

    add .gitignore, remove target from tracking

--- target/debug/deps/strategy_api-b8806cf5506f08bb.d
+++
@@ -1,8 +0,0 @@
-C:\Users\JianJian Li\strategy-api\target\debug\deps\strategy_api-b8806cf5506f08bb.d: src\lib.rs src\bar.rs src\draw.rs src\strategy.rs
-
-C:\Users\JianJian Li\strategy-api\target\debug\deps\libstrategy_api-b8806cf5506f08bb.rmeta: src\lib.rs src\bar.rs src\draw.rs src\strategy.rs
-
-src\lib.rs:
-src\bar.rs:
-src\draw.rs:
-src\strategy.rs:
