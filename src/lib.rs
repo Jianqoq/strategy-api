cc6f717ee7ce57285f432cd90952e628e5c4f3d1
JianJian Li <ljj1849532909@gmail.com>
2026-05-22 23:26:54 -0400

    a

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,11 @@ pub mod strategy;
 
 pub use bar::Bar;
 pub use ctx::{
-    Account, Annotation, AnnotationKind, Fill, FuturesAccount, Liquidation, Order,
-    OrderId, OrderKind, OrderStatus, Position, PositionId, Side, SpotAccount, Strategy, Trade,
+    Account, Annotation, AnnotationKind, Fill, FuturesAccount, Liquidation, Order, OrderId,
+    OrderKind, OrderStatus, Position, PositionId, Side, SpotAccount, Strategy, Trade,
 };
 
 pub use strategy::{
-    BUY, FnOnStrategyBar, FnOnInit, HOLD, SELL, Signal, StateBlob,
+    BUY, FnOnIndicatorBar, FnOnIndicatorFinish, FnOnInit, FnOnRestoreState, FnOnStrategyBar,
+    FnOnStrategyFinish, HOLD, OnIndicator, OnStrategy, SELL, Signal, StateBlob,
 };
